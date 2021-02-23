011b00c289d22a0c9da7fdce8f3c54e1b2176614
Allison <47452487+allisons11@users.noreply.github.com>
2021-02-23 16:17:46 -0500

    MainContainer no longer called Wrapper in App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -10,7 +10,7 @@
  */
 
 import React, { Component } from 'react';
-import Wrapper from './containers/MainContainer.jsx';
+import MainContainer from './containers/MainContainer.jsx';
 
 class App extends Component {
   constructor(props) {
@@ -20,7 +20,7 @@ class App extends Component {
   render() {
     return(
       <div>
-        <Wrapper/>
+        <MainContainer/>
       </div>
     );
   }
