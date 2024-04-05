65937e36584ec5b5ed7c0b89e10c3d75ce753de4
laurabotel <laura.e.botel@gmail.com>
2024-04-05 15:13:50 -0400

    adds redux toolkit and configureStore method to create the store, and note about legacy createStore method

--- client/store.js
+++ client/store.js
@@ -9,14 +9,25 @@
  * ************************************
  */
 
+// legacy redux dependencies
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './reducers/index';
 
+// rtk dependencies
+import { configureStore } from '@reduxjs/toolkit';
+import marketsReducer from './reducers/marketsReducer';
+
+// modern redux toolkit `configureStore()` -- https://redux-toolkit.js.org/api/configureStore
+const store = configureStore({
+  reducer: {
+    marketsReducer,
+    // if we had other reducers, they would go here
+  },
+});
+
+// legacy redux: `createStore()` -- https://redux.js.org/api/createstore
 // we are adding composeWithDevTools here to get easy access to the Redux dev tools
-const store = createStore(
-  reducers,
-  composeWithDevTools()
-);
+// const store = createStore(reducers, composeWithDevTools());
 
 export default store;
